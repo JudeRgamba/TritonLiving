49885c238ec44bcb5fe2bcde00c786667ce40d5d
Adrian Vo <adriankvo22@gmail.com>
2026-05-12 14:44:24 -0700

    feat: implement nextauth with google provider signin

---
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+
+    callbacks: {
+        async signIn({ user }) {
+            return user.email?.endsWith("ucsd.edu") ?? false;
+        },
+    },
+});
+
+export { handler as GET, handler as POST };

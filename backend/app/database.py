3919c48330c869e54673a449e81e9dedc509ea5a
Adrian Vo <adriankvo22@gmail.com>
2026-05-26 13:23:21 -0700

    setup docker

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 import os
+
+load_dotenv()  # Load environment variables from .env file
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine("DATABASE_URL")
+# engine is starting point of SQLAlchemy, it manages the connection pool
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.routers import users #imports users file from routers
+from app.database import engine, Base
+
+Base.metadata.create_all(bind=engine) #creates tables in database based on models defined in Base
 
 app = FastAPI()
 app.include_router(users.router)
